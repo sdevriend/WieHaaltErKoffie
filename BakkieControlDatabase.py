7cd25945e8db282c612a5400c338e2541ffd6631
sdevriend <sebastiaandevriend@gmail.com>
2016-01-04 22:25:46 +0100

    update controller
    controller heeft nu meer features!

--- BakkieControlDatabase.py
+++ BakkieControlDatabase.py
@@ -5,23 +5,34 @@ Om het database gedeelte eenvoudig te maken is het script in class vorm
 gezet om alles eenvoudig te beheren.
 
 Sebastiaan de Vriend 15-12-2015 Aanmaken script.
+Sebastiaan de Vriend 04-01-2016 Documentatie en toevoegen features.
 """
 import os
 import sqlite3
+from datetime import datetime
+from time import time
+
 
 class BakkieControlDatabase():
-    def __init__(self, check=False):
+    def __init__(self, check=False, testdata=False):
         """
         Input:
             check: Boolean: Bij True wordt er gekeken of de database
                             beschikbaar is en zoniet dan wordt deze
                             aangemaakt.
-    
+            testdata: Boolean: Bij True wordt er testdata ingevoerd.
+                               Aan te raden is om dit op True te
+                               zetten bij eerste keer startup van de
+                               applicatie.
         """
         self.db_filename = "Bakkie.db"
         if check:
             self.startup()
-        self.startConnectionDatabase()
+        self.startConnectionDatabase() #  Hierna datainvoeren.
+        if testdata:
+            self.addTestData()
+        
+        
         
     
     def startConnectionDatabase(self):
@@ -54,3 +65,85 @@ class BakkieControlDatabase():
             sqlfile.close()
             conn.commit()
             conn.close()
+    
+    def close(self):
+        """
+        De functie sluit de connectie met de database.
+        """
+        self.connection.commit()
+        self.connection.close()
+
+    def getUsers(self):
+        """
+        De functie haalt alle gebruikers op en plaatst deze 
+        """
+        self.cursor.execute('SELECT * FROM Gebruiker')
+        rows = self.cursor.fetchall()
+        users = []
+        for row in rows:
+            users.append((row[0], str(row[1])))
+        users.sort(key=lambda x: x[1])
+        return users
+    
+    def addTestData(self):
+        """
+        De functie kijkt of er al data in de database zit. Als er geen
+        data in de gebruikersdatabase zit, dan wordt er testdata
+        queries ingeladen en deze worden vervolgens in de database
+        geplaatst. Na het schrijven van de data, wordt de transactie
+        voltooid.
+        """
+        self.cursor.execute('SELECT * FROM Gebruiker')
+        testusers = ["Sebastiaan", "Jesse", "Dwin", "Jolanda", "Jeroen"]
+        if len(self.cursor.fetchall()) == 0:
+            map(self.addUser, testusers)
+            #sqlfile = open("testdata.txt")
+            #sql = sqlfile.read()
+            #self.cursor.executescript(sql)
+            #sqlfile.close()
+            #self.connection.commit()
+            # Is voor echte testdata.
+
+    def addUser(self, username):
+        """
+        Input: 1
+            username: String: Gebruikersnaam van nieuwe gebruiker.
+        Het script voegd de nieuwe gebruiker toe aan de database en
+        commit de transactie. Daarna wordt de username in een bericht
+        geplaatst en deze wordt doorgestuurd naar de writelog functie.
+        """
+        self.cursor.execute('INSERT INTO Gebruiker (naam) VALUES(?)', (username,))
+        self.connection.commit()
+        bericht = "Gebruiker " + username + " is aangemaakt."
+        self.__writelog(bericht)
+        
+    def __writelog(self, bericht):
+        """
+        Input: 1
+            bericht: str: Bericht die in de database geplaatst wordt.
+        De functie maakt een epoch timestamp en haalt de miliseconde af
+        en plaatst deze met het bericht in de log database.
+        """
+        tijd = int(time())
+        self.cursor.execute('INSERT INTO Log (Datum, Bericht) VALUES(?, ?)', (tijd, bericht, ))
+        self.connection.commit()
+    
+    def deleteUser(self, username):
+        """
+        Input: 1 
+            username: str: Gebruikersnaam die verwijderd kan worden uit
+                           de database.
+        De functie verwijderd uit de database de gebruiker. en commit
+        daarna de transactie. Vervolgens wordt er een bericht
+        aangemaakt en doorgestuurd naar writelog.
+        """
+        self.cursor.execute('DELETE FROM Gebruiker WHERE Naam = ?', (username,)) # mogelijk cascade onderzoeken!
+        self.connection.commit()
+        bericht = "Gebruiker" + username + " is verwijderd."
+        self.__writelog(bericht)
+    
+    def getPrijzenlijst(self):
+        self.cursor.execute('SELECT * FROM Prijzenlijst')
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print row
--- testdb.py
+++ testdb.py
@@ -1,3 +1,23 @@
 from BakkieControlDatabase import BakkieControlDatabase
 
-a = BakkieControlDatabase(True)
+a = BakkieControlDatabase(True, True)
+
+
+print "Doe hier je acties"
+print "Voeg klaas toe."
+a.addUser("Klaas")
+a.addUser("Klaa")
+a.addUser("Klaasje")
+print "Show users: "
+users = a.getUsers()
+print users
+print "--------"
+print "Verwijder Klaas"
+a.deleteUser("Klaas")
+users = a.getUsers()
+print users
+print "--------------"
+print "Prijzenlijst:"
+a.getPrijzenlijst()
+print "Sluit daarna de db"
+a.close()
